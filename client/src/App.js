199d5a6528046c9d5e38a547447b78425f3fbf38
David Jungst <djungst@gmail.com>
2018-09-20 12:32:07 -0600

    auth stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import NoMatch from './components/NoMatch'
 import NavBar from './components/NavBar'
 import Login from './components/Login'
 import Register from './components/Register'
+import ProtectedRoute from './components/ProtectedRoute'
+import FetchUser from './components/FetchUser'
 import { Switch, Route } from 'react-router-dom'
 
 const App = () => (
   <Fragment>
     <NavBar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route component={NoMatch} />
-    </Switch>
+    <FetchUser>
+      <Switch>
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NoMatch} />
+      </Switch>
+    </FetchUser>
   </Fragment>
 )
 
